3a490e584a17d53df57373c9f9893d29956994b6
NitinKarthik01 <nitinsrikarthik@gmail.com>
2026-08-26 15:51:39 +0530

    Linked List

---
+++ Linked_List/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "slist.h"
+
+void test()
+{
+    List *l1 = slist_new();
+
+    if (l1 == NULL)
+    {
+        printf("Failed to create list.\n");
+        return;
+    }
+
+    slist_add_head(l1, 67);
+    slist_add_head(l1, 33);
+    slist_add_head(l1, 54);
+    slist_add_head(l1, 90);
+    slist_add_head(l1, 21);
+
+    display_list(l1);
+
+    slist_add_tail(l1, 5);
+    slist_add_tail(l1, 1);
+    slist_add_tail(l1, 8);
+
+    display_list(l1);
+
+    slist_delete_head(l1);
+    slist_delete_head(l1);
+
+    display_list(l1);
+
+    /*
+    slist_delete_tail(l1);
+    slist_delete_tail(l1);
+
+    display_list(l1);
+    */
+
+    slist_free(l1);
+}
+
+int main()
+{
+    test();
+
+    return 0;
+}
---
+++ Linked_List/slist.c
@@ -0,0 +1,227 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "slist.h"
+
+static Node *_list_new_node_(uint32_t data)
+{
+    Node *node = (Node *)malloc(sizeof(Node));
+
+    if (node == NULL)
+        return NULL;
+
+    node->data = data;
+    node->next = NULL;
+
+    return node;
+}
+
+List *slist_new()
+{
+    List *list = (List *)malloc(sizeof(List));
+
+    if (list == NULL)
+        return NULL;
+
+    list->head = NULL;
+    list->tail = NULL;
+    list->length = 0;
+
+    return list;
+}
+
+List *slist_free(List *list)
+{
+    Node *p;
+
+    if (list == NULL)
+        return NULL;
+
+    while (list->head != NULL)
+    {
+        p = list->head;
+        list->head = list->head->next;
+        free(p);
+    }
+
+    list->tail = NULL;
+    list->length = 0;
+
+    free(list);
+
+    return NULL;
+}
+
+uint32_t slist_length(List *list)
+{
+    if (list == NULL)
+        return 0;
+
+    return list->length;
+}
+
+Node *slist_lookup(List *list, uint32_t key)
+{
+    if (list == NULL)
+        return NULL;
+
+    Node *node = list->head;
+
+    for (; node != NULL; node = node->next)
+    {
+        if (node->data == key)
+            return node;
+    }
+
+    return NULL;
+}
+
+List *slist_add_head(List *list, uint32_t data)
+{
+    if (list == NULL)
+        return NULL;
+
+    Node *new_node = _list_new_node_(data);
+
+    if (new_node == NULL)
+        return list;
+
+    if (list->head == NULL)
+    {
+        list->head = list->tail = new_node;
+    }
+    else
+    {
+        new_node->next = list->head;
+        list->head = new_node;
+    }
+
+    ++list->length;
+
+    return list;
+}
+
+List *slist_add_tail(List *list, uint32_t data)
+{
+    if (list == NULL)
+        return NULL;
+
+    Node *new_node = _list_new_node_(data);
+
+    if (new_node == NULL)
+        return list;
+
+    if (list->head == NULL)
+    {
+        list->head = list->tail = new_node;
+    }
+    else
+    {
+        list->tail->next = new_node;
+        list->tail = new_node;
+    }
+
+    ++list->length;
+
+    return list;
+}
+
+List *slist_delete_head(List *list)
+{
+    if (list == NULL)
+        return NULL;
+
+    Node *node;
+
+    if (list->head != NULL)
+    {
+        node = list->head;
+        list->head = list->head->next;
+
+        if (list->head == NULL)
+        {
+            list->tail = NULL;
+        }
+
+        free(node);
+        --list->length;
+    }
+
+    return list;
+}
+
+List *slist_delete_tail(List *list)
+{
+    if (list == NULL || list->head == NULL)
+        return list;
+
+    if (list->head == list->tail)
+    {
+        free(list->head);
+        list->head = NULL;
+        list->tail = NULL;
+        list->length = 0;
+
+        return list;
+    }
+
+    Node *current = list->head;
+
+    while (current->next != list->tail)
+    {
+        current = current->next;
+    }
+
+    free(list->tail);
+
+    list->tail = current;
+    list->tail->next = NULL;
+
+    --list->length;
+
+    return list;
+}
+
+List *add_in_between(List *list, uint32_t key, uint32_t data)
+{
+    if (list == NULL)
+        return NULL;
+
+    Node *current = slist_lookup(list, key);
+
+    if (current == NULL)
+        return list;
+
+    Node *new_node = _list_new_node_(data);
+
+    if (new_node == NULL)
+        return list;
+
+    new_node->next = current->next;
+    current->next = new_node;
+
+    if (current == list->tail)
+        list->tail = new_node;
+
+    ++list->length;
+
+    return list;
+}
+
+void display_list(List *list)
+{
+    if (list == NULL)
+    {
+        printf("\nList is NULL\n");
+        return;
+    }
+
+    printf("\nLINKEDLIST{\n\t");
+
+    for (Node *node = list->head; node != NULL; node = node->next)
+    {
+        printf("%u -> ", node->data);
+    }
+
+    printf("NULL\n}\n");
+}
